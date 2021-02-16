43e56b4c0268ae011d8221759adb6ddcb0a6c9d8
nataly garcia <naty_garcioli@hotmail.com>
2021-02-16 14:57:01 -0500

    added delete method & alert

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See all task // .all
+  get 'tasks', to: 'tasks#index'
+  # [GET] Create a task // new  = 1 request
+  get 'tasks/new', to: 'tasks#new'
+  # See details of a task // por id // se cambia porque tiene el path dinamic
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # [POST] the parameters to create a new task = 1 request
+  post 'tasks', to: 'tasks#create'
+  # Update a task // edit = 1 request
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # Update // Patch paso 1.1
+  patch 'tasks/:id', to: 'tasks#update'
+  # Destroy a task
+  delete 'task/:id', to: 'tasks#destroy', as: :destroy_task
 end
